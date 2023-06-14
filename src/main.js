ef560b8b7dc4a6d09298c2cf117ea5ecd862b650
MyYoungAngryGirl <1745551833@qq.com>
2023-06-14 10:53:15 +0800

    静态资源引入和插件安装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入初始化样式文件
+import '@/styles/common.scss'
 // 测试接口函数
 import { getCategory } from '@/apis/testAPI'
 getCategory().then(res=>{
@@ -13,6 +15,7 @@ getCategory().then(res=>{
 })
 
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
