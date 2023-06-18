d02b69033b95436d6a17b93e498112264ac07313
MyYoungAngryGirl <1745551833@qq.com>
2023-06-18 15:02:32 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ import router from './router'
 import '@/styles/common.scss'
 // 引入懒加载指令插件并且注册
 import {lazyPlugin} from '@/directives'
-
+// 引入全局组件插件
+import {componentPlugin} from '@/components'
 
 const app = createApp(App)
 
@@ -18,4 +19,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
