f0159b161b84d82ef9c99ded5b7b14ac7afb3402
MyYoungAngryGirl <1745551833@qq.com>
2023-06-24 17:05:13 +0800

    购物车-本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,11 +1,11 @@
 // 封装购物车模块
-import {defineStore} from 'pinia'
-import {computed, ref} from 'vue'
-export const useCartStore = defineStore ('cart',()=>{
-    // 1.定义state-cartList
-    const cartList = ref([])
-    // 2.定义action-addCart
-    const addCart = (goods)=>{
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+export const useCartStore = defineStore('cart', () => {
+  // 1.定义state-cartList
+  const cartList = ref([])
+  // 2.定义action-addCart
+  const addCart = (goods) => {
     // 添加购物车操作
     // 已添加过 count+1
     // 没有添加过 直接push
@@ -18,36 +18,49 @@ export const useCartStore = defineStore ('cart',()=>{
       // 没找到
       cartList.value.push(goods)
     }
-  }    
+  }
 
   // 删除购物车
-  const delCart = (skuId)=>{
+  const delCart = (skuId) => {
     // 思路：1.找到要删除项的下标值 - splice
     // 2.使用数组的过滤方法 - filter
-    const idx = cartList.value.findIndex((item)=>skuId === item.skuId)
-    cartList.value.splice(idx,1)
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
   }
 
   // 单选功能
-  const singleCheck = (skuId,selected)=>{
+  const singleCheck = (skuId, selected) => {
     // 通过skuId找到要修改的那一项，然后把它的selected修改为传过来的selected
-    const item = cartList.value.find((item)=>item.skuId === skuId)
+    const item = cartList.value.find((item) => item.skuId === skuId)
     item.selected = selected
   }
-  
+
+  // 全选功能
+  const allCheck = (selected)=>{
+    // 把cartList中的每一项的selected都设置为当前的全选框状态
+    cartList.value.forEach(item=>item.selected = selected)
+  }
+
   //计算属性
   // 1.总的数量 所有项的count之和
-  const allCount = computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
   // 2.总价 所有项的count*price之和 
-  const allPrice = computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
-    return {
-        cartList,
-        allCount,
-        allPrice,
-        addCart,
-        delCart,
-        singleCheck
-    }
-},{
-    persist:true
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+  
+  // 是否全选
+  const isAll = computed(()=>cartList.value.every((item)=>item.selected))
+
+
+  return {
+    cartList,
+    allCount,
+    allPrice,
+    isAll,
+    addCart,
+    delCart,
+    singleCheck,
+    allCheck
+  }
+}, {
+  persist: true
 })
